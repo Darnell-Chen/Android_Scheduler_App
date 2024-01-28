a140b76ada3f7699f56bb1bd2ba6da91ac634778
bbdavidson11 <bbdavidson11@gmail.com>
2024-01-28 13:21:57 -0500

    changes

--- app/src/main/java/com/example/cs2340proj1/ui/todo/TodoListViewModel.java
+++ app/src/main/java/com/example/cs2340proj1/ui/todo/TodoListViewModel.java
@@ -1,14 +1,14 @@
-package com.example.cs2340proj1.ui.dashboard;
+package com.example.cs2340proj1.ui.todo;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class TodoListViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public TodoListViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
